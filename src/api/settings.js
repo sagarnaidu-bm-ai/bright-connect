cbbb17a95d789d37f365b43e452cc94fbadeb874
sagar naidu <sagar@ip-192-168-0-3.us-west-2.compute.internal>
2026-03-29 22:32:52 +0530

    Fix SMS frequency capping to show Transactional only

--- src/api/settings.js
+++ src/api/settings.js
@@ -8,7 +8,7 @@ const SETTINGS_MOCK = {
   frequencyCapping: [
     { channel: 'Email', maxPerDay: 2, maxPerWeek: 5, appliesTo: 'Marketing only' },
     { channel: 'Push', maxPerDay: 3, maxPerWeek: 10, appliesTo: 'All campaigns' },
-    { channel: 'SMS', maxPerDay: 1, maxPerWeek: 3, appliesTo: 'Marketing only' },
+    { channel: 'SMS', maxPerDay: 1, maxPerWeek: 3, appliesTo: 'Transactional only' },
   ],
   unsubscribe: {
     email: true,
